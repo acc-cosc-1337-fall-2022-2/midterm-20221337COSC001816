ebcd50ab9f65df4ba62ed5aea4963c75ac165947
20221337COSC001816 <112662502+20221337COSC001816@users.noreply.github.com>
2022-10-15 23:28:56 -0500

    Exam 1

--- src/question_3/question3.cpp
+++ src/question_3/question3.cpp
@@ -2,7 +2,7 @@
 #include <string>
 using namespace std;
 #include <vector>
-
+#include <iostream>
 
 
 bool test_config()
@@ -13,7 +13,30 @@ bool test_config()
 
 string get_fib_sequence(int parameterMaximumValue)
 {
+    string finalSequence;
+    string integerToString;
+    int nextValue;
+    int firstValue = 0;
+    int secondValue = 1;
 
 
-    
+    finalSequence.append("0 1 ");
+
+
+    for(int i = 1; i <= parameterMaximumValue; i++)
+    {
+
+
+        nextValue = firstValue + secondValue;//The next value is equal to the previous values
+
+        firstValue = secondValue;
+        secondValue = nextValue;
+
+
+        integerToString = to_string(nextValue);
+        finalSequence.append(integerToString);
+        finalSequence.append(" ");
+    }
+
+    return finalSequence;
 }
